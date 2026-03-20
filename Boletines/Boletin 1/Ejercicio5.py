2583d4479714cf0fcb040e2c35ec05d99fa31aa0
Adrián Álvarez Bombín <adrialvarezbom@gmail.com>
2026-03-20 09:17:30 +0100

    Delete Boletines/Boletin 1 directory

--- Boletines/Boletin 1/Ejercicio5.py
+++
@@ -1,6 +0,0 @@
-# Escribir un programa que pida por teclado un número al usuario y diga si es par o impar
-numero = int(input("Dame un numero: "))
-if numero % 2 == 0:
-    print("Es par")
-else:
-    print("Es impar")
