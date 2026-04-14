3c78469219af953e489920f2428c2de020b5e059
Adrián Álvarez Bombín <adrialvarezbom@gmail.com>
2026-04-14 09:41:46 +0200

    Add files via upload

--- Boletines/Boletin 5/Ejercicios/Ejercicio 4.py
+++ Boletines/Boletin 5/Ejercicios/Ejercicio 4.py
@@ -4,3 +4,21 @@
 # de decir que 3. Tendrías que tener en cuenta que las vocales pueden ir en mayúsculas
 # o no y son la misma letra. Presupón que ninguna vocal va acentuada de ninguna
 # forma.
+
+frase = input("Ingrese una frase: ")
+
+palabras = frase.split()
+contador = 0
+
+for palabra in palabras:
+    palabra = palabra.lower()
+    vocales_encontradas = []
+
+    for letra in palabra:
+        if letra in "aeiou" and letra not in vocales_encontradas:
+            vocales_encontradas.append(letra)
+
+    if len(vocales_encontradas) >= 4:
+        contador += 1
+
+print("Cantidad de palabras con 4 o más vocales diferentes:", contador)
