fb911a8e62480597b06315121ca5cdcad855c8df
Adrián Álvarez Bombín <adrialvarezbom@gmail.com>
2026-04-21 12:05:28 +0200

    Delete Boletines/Boletines de Pyhon/Boletin 3/Ejercicios directory

--- Boletines/Boletines de Pyhon/Boletin 3/Ejercicios/Ejercicio 2.py
+++
@@ -1,8 +0,0 @@
-# 2. Escribir un programa que pida al usuario que escriba una cadena de texto y la imprima
-# escrita al reves (es decir, si el usuario escribe Hola Mundo el programa debería de
-# escribir odnuM aloH)
-
-texto = input("Introduce una frase: ")
-
-cadena_invertida = texto[::-1]
-print(cadena_invertida)
