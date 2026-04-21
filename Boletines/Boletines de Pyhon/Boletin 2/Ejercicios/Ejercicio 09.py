926e1b791a4b2fbcd7e43b54efd7d270fcdbfdec
Adrián Álvarez Bombín <adrialvarezbom@gmail.com>
2026-04-21 12:03:12 +0200

    Add files via upload

---
+++ Boletines/Boletines de Pyhon/Boletin 2/Ejercicios/Ejercicio 09.py
@@ -0,0 +1,8 @@
+# 9. Escribir un programa en python que nos pida nuestro nombre y apellidos (dos peticiones
+# hechas en ese orden) y nos lo escriba formateado de la siguiente forma:
+# Morales Vázquez, José María
+
+nombre = input("Introduce tu nombre: ")
+apellido = input("Introduce tu apellido: ")
+
+print(apellido,"," ,nombre)
