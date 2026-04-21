335cce543921f54e49a3f1a4d2b7633935bf1a2b
Adrián Álvarez Bombín <adrialvarezbom@gmail.com>
2026-04-21 12:15:24 +0200

    Delete Boletines/Boletines de Java en Python/Boletin 3/Ejercicios directory

--- Boletines/Boletines de Java en Python/Boletin 3/Ejercicios/Ejercicio 3.py
+++
@@ -1,8 +0,0 @@
-# Escribir un programa que nos pida nuestro nombre y apellidos (dos peticiones
-# diferentes hechas en ese orden) y nos lo escriba formateado de la siguiente forma:
-# Morales Vázquez, José María
-
-nombre = input("Introduce tu nombre: ")
-apellidos = input("Introduce tu apellido: ")
-
-print(f"{apellidos}, {nombre}")
