e6e011663d26c6f0a8b5ddabb21dd75cb6a24e1c
Adrián Álvarez Bombín <adrialvarezbom@gmail.com>
2026-04-21 12:06:41 +0200

    Delete Boletines/Boletines de Pyhon/Boletin 4/Ejercicios directory

--- Boletines/Boletines de Pyhon/Boletin 4/Ejercicios/Ejercicio 1.py
+++
@@ -1,9 +0,0 @@
-# 1. Escribir un programa en python que pida al usuario un número y escriba todos sus
-# divisores
-
-numero = int(input("Introduce un número: "))
-print(f"Los divisores de {numero} son: ")
-
-for i in range(1, numero):
-    if numero % i == 0:
-        print(i)
