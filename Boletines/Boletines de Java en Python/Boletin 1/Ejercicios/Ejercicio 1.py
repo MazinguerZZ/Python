f3547007355b1dfecabddcdb2fd4481f97d8208f
Adrián Álvarez Bombín <adrialvarezbom@gmail.com>
2026-04-21 12:10:16 +0200

    Delete Boletines/Boletines de Java en Python/Boletin 1/Ejercicios directory

--- Boletines/Boletines de Java en Python/Boletin 1/Ejercicios/Ejercicio 1.py
+++
@@ -1,3 +0,0 @@
-# Escribir un programa donde se muestren los 10 primeros números enteros
-for n in range(0,10):
-    print(n)
--- Boletines/Boletines de Java en Python/Boletin 1/Ejercicios/Ejercicio 4.py
+++
@@ -1,4 +0,0 @@
-# Escribir un programa donde se muestren todos los números divisibles por 7 menores a 10000
-for n in range(1,10001):
-    if n % 7 == 0 :
-        print(n)
